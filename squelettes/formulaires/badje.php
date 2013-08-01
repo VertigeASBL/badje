421709d03e64b26dd7ead39e9aee3753b1b39a0e
phenix-factory <trash@henix.be>
2013-08-01 17:27:29 +0200

    On ajoute le formulaire de séléction de période.

--- squelettes/formulaires/badje.php
+++ squelettes/formulaires/badje.php
@@ -33,28 +33,57 @@ function formulaires_badje_charger_dist() {
     *   Les recherches ce feront ainsi sur le code postal qui est une donnée plus fiable que le nom de la commune.
     */
     $commune = array('all' => 'Toutes les communes');
+    
     // On récupère les donnée de la base de donnée des organiseme
     $organisme_commune = sql_allfetsel('code_postal, localite', 'spip_badje_organismes', '', 'code_postal');
+    
     // On boucle sur le retour de base de donnée pour l'ajouter à notre trableau
     foreach ($organisme_commune as $key => $value) {
         $commune[$value['code_postal']] = $value['localite'].' '.$value['code_postal'];
     }
+
+    // La liste des périodes disponible.
+    $periodes = array(
+        'Automne' => 'Automne', 
+        'Hiver' => 'Hiver',
+        'Détente' => 'Détente', 
+        'Printemps' => 'Printemps', 
+        'Juillet' => 'Juillet', 
+        'Août' => 'Août'
+    );
 
     $form_saisie_options = array( 
         array(
             'saisie' => 'fieldset',
             'options' => array(
                 'nom' => 'lieux', 
-                'label' => 'Lieux'),
+                'label' => 'Lieux'
+                ),
             'saisies' => array(
                 array('saisie' => 'checkbox',
                     'options' => array(
                         'nom' => 'lieux', 
                         'datas' => $commune
                         )
+                    )
+                )
+        ),
+        array(
+            'saisie' => 'fieldset',
+            'options' => array(
+                'nom' => 'periodes',
+                'label' => 'Périodes'
+                ),
+            'saisies' => array(
+                array(
+                    'saisie' => 'checkbox',
+                    'options' => array(
+                        'nom' => 'periodes',
+                        'datas' => $periodes
+                        )
+                    )
                 )
             )
-        )
     );
 
     $contexte = array(
