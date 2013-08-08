d5aa7aa0e621b495ca6c585becc24843aa827c34
phenix-factory <trash@henix.be>
2013-08-08 12:25:29 +0200

    On affiche le titre de la page de recherche (nombre d'annonce). On vire les détails car on va les refaires.

--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -3,6 +3,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
     // A
+    'activite_trouve' => 'Activité trouvée',
+    'activites_trouves' => 'Activités trouvées',
     // B
     // C
     // D
