ea738c3d6eb97ea6856e50d4939156815c74dfe0
phenix-factory <trash@henix.be>
2013-08-22 12:47:59 +0200

    On vire le code de débug...

--- plugins/fabrique_auto/badje/badje_fonctions.php
+++ plugins/fabrique_auto/badje/badje_fonctions.php
@@ -44,7 +44,7 @@ function add_http($url) {
 *   Certains champs sont des des tableaux que l'ont dois assembler avant de les ajouter à la base de donnée.
 */
 function implode_champs() {
-    var_dump(_request('logement'));
+    
     // On traite le champ logement
     if (_request('logement')) 
         set_request('logement', implode(',', _request('logement')));
