fc6e9d5d9eff6ff87b3875281d2e288092d3788a
phenix-factory <trash@henix.be>
2013-08-29 17:13:30 +0200

    Faute de frappe... Nawak !

--- plugins/fabrique_auto/badje/badje_fonctions.php
+++ plugins/fabrique_auto/badje/badje_fonctions.php
@@ -36,7 +36,7 @@ function get_array_periode($periode_string) {
 */
 function add_http($url) {
     if (!preg_match("/^(http|https|ftp):/", $url) and $url) 
-        return $url = 'http://'.$url;X
+        return $url = 'http://'.$url;
 }
 
 
