7d0c4d2e4b08194e19512f86b2aa374f46d64cf3
phenix-factory <trash@henix.be>
2013-09-10 16:16:03 +0200

    Ajoute la fonction d'export par commune

--- plugins/fabrique_auto/ATL/formulaires/exporter_organisme.php
+++ plugins/fabrique_auto/ATL/formulaires/exporter_organisme.php
@@ -1,6 +1,17 @@
 <?php
 function formulaires_exporter_organisme_charger_dist() {
-    $contexte = array();
+    
+    // On récupère les donnée de la base de donnée des organiseme
+    $organisme_commune = sql_allfetsel('code_postal, commune', 'spip_badje_activites', '', 'code_postal');
+    
+    // On boucle sur le retour de base de donnée pour l'ajouter à notre tableau
+    foreach ($organisme_commune as $key => $value) {
+        $commune[$value['code_postal']] = $value['commune'].' '.$value['code_postal'];
+    }
+
+    asort($commune);
+
+    $contexte = array('commune' => $commune);
     return $contexte;
 }
 
@@ -17,11 +28,32 @@ function formulaires_exporter_organisme_verifier_dist() {
 
 function formulaires_exporter_organisme_traiter_dist() {
     
-    // On récupère la table des organismes
-    $sql = sql_select('*', 'spip_badje_organismes');
+    // On déclare le tableau qui sera en réalité le fichier CSV
+    $csv = array();
 
+    // On récupère les champs de la table
+    $champ = lister_tables_objets_sql('spip_badje_activites');
+    
+    // On ajoute les champs en première ligne du fichier csv.
+    $csv[] = $champ['champs_editables'];
+
+    // Si une commune est envoyer on filtre sur cette commune.
+    if (_request('commune_export')) 
+        $where = 'code_postal='.sql_quote(_request('commune_export'));
+    // Sinon pas de filtre
+    else $where = null;
+    
+    // On récupère les activités.
+    $data = sql_allfetsel($champ['champs_editables'], 'spip_badje_activites', $where);
+
+    // on ajoute les donnée des activité au fichier csv.
+    $csv = array_merge($csv, $data);
+
+    // On va chercher la fonction de création de fichier CSV
     $exporter_csv = charger_fonction('exporter_csv', 'inc/', true);
-    echo $exporter_csv('organisme', $sql);
+    // On crée le fichier CSV
+    echo $exporter_csv('export_activite_'._request('commune_export'), $csv);
+    // On arrête tout, le fichier est envoyé a l'utilisateur.
     exit();
 
     /* message */
--- plugins/fabrique_auto/ATL/lang/ATL_fr.php
+++ plugins/fabrique_auto/ATL/lang/ATL_fr.php
@@ -17,7 +17,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
     'deconnexion' => 'Déconnexion',
     // E
     'explication_type_activite' => 'Choisissé un ou plusieurs type d\'activité',
-    'export' => 'Exporter les organismes',
+    'export' => 'Exporter les activités.',
     // G
     'general' => 'Général',
     // L
