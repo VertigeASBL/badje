a7e0750a4974652c17ec716cf45da626c94f1e8e
phenix-factory <trash@henix.be>
2013-09-12 17:38:47 +0200

    Fonction de suppression du caddie.

--- squelettes/formulaires/cadi.php
+++ squelettes/formulaires/cadi.php
@@ -28,7 +28,9 @@ function formulaires_cadi_verifier_dist($id_activite) {
     $cadi_actuel = session_get('cadi');
 
     // S'il existe et que l'activité est déjà dans le tableau, on passe dans le contexte une instruction pour ne pas afficher le formulaire.
-    if (is_array($cadi_actuel) and in_array($id_activite, $cadi_actuel)) {
+    if (is_array($cadi_actuel) 
+        and in_array($id_activite, $cadi_actuel)
+        and !_request('delete')) {
         $erreurs['message_erreur'] = 'Déjà dans le cadi';
     }
 
@@ -37,20 +39,31 @@ function formulaires_cadi_verifier_dist($id_activite) {
 
 function formulaires_cadi_traiter_dist($id_activite) {
     
-    // On récupère le cadi actuel du visiteur
-    $cadi_actuel = session_get('cadi');
-
-    // On place le nouvel objet
-    $cadi_actuel[] = $id_activite;
-
-    // Mise à jour de la session
-    session_set('cadi', $cadi_actuel);
-
-    // All clear !
-    return array(
+    $res = array(
             'editable' => true,
             'message_ok' => '',
             'redirect' => ''
     );
+
+    // On récupère le cadi actuel du visiteur
+    $cadi_actuel = session_get('cadi');
+
+    // Si on ne supprime pas, on ajoute
+    if (!_request('delete')) {
+        // On place le nouvel objet
+        $cadi_actuel[] = $id_activite;
+
+        // Mise à jour de la session
+        session_set('cadi', $cadi_actuel);
+    }
+    // Sinon on supprime
+    else {
+        unset($cadi_actuel[array_search($id_activite, $cadi_actuel)]);
+
+        session_set('cadi', $cadi_actuel);
+    }
+
+    // All clear !
+    return $res;
 }
 ?>
