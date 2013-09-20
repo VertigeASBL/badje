499a08a21a7557aa0280265f7875a7edb58a0509
phenix-factory <trash@henix.be>
2013-09-20 16:41:59 +0200

    Ajouter les type d'activités au CSV

--- plugins/fabrique_auto/ATL/formulaires/exporter_organisme.php
+++ plugins/fabrique_auto/ATL/formulaires/exporter_organisme.php
@@ -33,18 +33,47 @@ function formulaires_exporter_organisme_traiter_dist() {
 
     // On récupère les champs de la table
     $champ = lister_tables_objets_sql('spip_badje_activites');
-    
+
     // On ajoute les champs en première ligne du fichier csv.
     $csv[] = $champ['champs_editables'];
+
+    // On ajoute manuellement la colonne "type d'activité"
+    $csv[0]['type_activite'] = "Type d'activite";
 
     // Si une commune est envoyer on filtre sur cette commune.
     if (_request('commune_export')) 
         $where = 'code_postal='.sql_quote(_request('commune_export'));
     // Sinon pas de filtre
     else $where = null;
-    
+
+    // En plus des champs éditable, on va ajouter l'id_activite.
+    $champ['champs_editables'][] = 'id_activite';
+
     // On récupère les activités.
     $data = sql_allfetsel($champ['champs_editables'], 'spip_badje_activites', $where);
+
+    // On va ajouter les types d'activité a la fin.
+    foreach ($data as $key => $value) {
+        
+        // On récupère les type d'activité.
+        $activite = sql_allfetsel(
+            'type.type_activite',
+            "spip_badje_type_activites_liens as liens
+                INNER JOIN spip_badje_type_activites as type
+                    ON type.id_type_activite = liens.id_type_activite",
+            'id_objet = '.sql_quote($value['id_activite']));
+
+        // La requête est faite, on supprime l'ID pour éviter de l'avoir dans le csv
+        unset($data[$key]['id_activite']);
+
+        // On va créer une chaine à mettre dans le CSV final.
+        $type_activite = '';
+        foreach ($activite as $cle => $valeur) {
+            $type_activite .= $valeur['type_activite'].', ';
+        }
+        spip_log($type_activite, 'type');
+        $data[$key]['type_activite'] = $type_activite;
+    }
 
     // on ajoute les donnée des activité au fichier csv.
     $csv = array_merge($csv, $data);
@@ -57,8 +86,8 @@ function formulaires_exporter_organisme_traiter_dist() {
 
     // On va chercher la fonction de création de fichier CSV
     $exporter_csv = charger_fonction('exporter_csv', 'inc/', true);
-    // On crée le fichier CSV
 
+    // On crée le fichier CSV
     echo $exporter_csv('export_activite_'._request('commune_export'), $csv, 'TAB');
     // On arrête tout, le fichier est envoyé a l'utilisateur.
     exit();
